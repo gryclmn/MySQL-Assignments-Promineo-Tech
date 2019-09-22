17839ae22a495287b35bc87586beabf7165b4f71
Gary Coleman <gryclmn@hotmail.com>
2019-09-22 11:05:56 -0700

    Ensured Week 2 assignment had all solutions.

--- Week 2 MySQL Assignment.sql
+++ Week 2 MySQL Assignment.sql
@@ -7,9 +7,21 @@ SELECT titles.title as Title,COUNT(*) as count
     GROUP BY titles.title 
     ORDER BY count DESC;
     
-    
-show tables;
-desc employees;
-desc titles;
 # 2. I want to know the average salary per title.
+SELECT title AS Title, avg(salary) AS "Average Salary"
+	FROM salaries
+	INNER JOIN titles
+	ON salaries.emp_no = titles.emp_no
+	GROUP BY title;
+
 # 3. How much money was spent on salary for the marketing department between the years 1990 and 1992?
+SELECT dept_name, sum(salary) AS Total
+	FROM salaries 
+	INNER JOIN dept_emp
+	ON salaries.emp_no = dept_emp.emp_no
+		INNER JOIN departments 
+		ON dept_emp.dept_no = departments.dept_no 
+	WHERE departments.dept_name = "Marketing"
+		AND (YEAR(salaries.from_date) BETWEEN "1990" AND "1992"
+		OR YEAR(salaries.to_date) BETWEEN "1990" AND "1992")
+	GROUP BY dept_emp.dept_no;
